715cbd2cd05b9901b9885700df427c855d16aab5
Dasun47 <dasunlakshan47@gmail.com>
2023-04-21 09:51:18 +0530

    Handling POST Requests

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { ObjectId } = require("mongodb");
 //init app & middleware
 
 const app = express();
+app.use(express.json());
 
 //db connection
 let db;
@@ -48,4 +49,17 @@ app.get("/books/:id", (req, res) => {
   } else {
     res.status(500).json({ error: "Not a valid doc id" });
   }
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "Could not create a new document" });
+    });
 });
